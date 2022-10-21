14342d27c1026b61bb0faf106813697e5a227b8f
ahmedmeid99 <you@example.com>
2022-10-21 20:20:28 +0200

    add-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '01-first-project';
+  open: Boolean = false;
+
+  openMenue() {
+    this.open = true;
+  }
+  closeMenue() {
+    this.open = false;
+  }
 }
